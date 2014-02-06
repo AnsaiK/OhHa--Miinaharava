6af0c1ae34d869425e771a56aad69716f8115327
AnssiK <anssi.kienanen@cs.helsinki.fi>
2014-02-06 19:11:40 +0200

    pientä viilausta

--- OhHaMiinaharava/src/main/java/Miinaharava/Kayttoliittyma/Kayttoliittyma.java
+++ OhHaMiinaharava/src/main/java/Miinaharava/Kayttoliittyma/Kayttoliittyma.java
@@ -10,33 +10,38 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
+/**
+ * Pelin graafinen käyttöliittymä. Vielä täysin vaiheessa..
+ *
+ */
 public class Kayttoliittyma extends JFrame implements Runnable {
 
     private JFrame frame;
     private Pelilauta lauta;
     private Pelilogiikka logiikka;
-    private Map<Ruutu, JButton> kaikkiRuudut;
+//    private Map<Ruutu, JButton> kaikkiRuudut;
 
     public Kayttoliittyma(Pelilogiikka logiikka) {
         this.logiikka = logiikka;
         this.lauta = logiikka.getPelilauta();
-        this.kaikkiRuudut = new HashMap();
+//        this.kaikkiRuudut = new HashMap();
     }
 
     public void run() {
         frame = new JFrame("Miinaharava");
 
-        frame.setPreferredSize(new Dimension(300, 320));
+        frame.setPreferredSize(new Dimension(300, 350));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         luoKomponentit(frame.getContentPane());
@@ -50,15 +55,20 @@ public class Kayttoliittyma extends JFrame implements Runnable {
 
         JMenuBar valikko = luoValikko();
         JPanel pelialue = luoPelialue();
+        JPanel tekstiKenttaAla = luoAlaKentta();
 
         container.setLayout(new BorderLayout());
 
         frame.setJMenuBar(valikko);
         container.add((pelialue), BorderLayout.CENTER);
-
+        container.add((tekstiKenttaAla), BorderLayout.SOUTH);
 
     }
 
+    /**
+     * Pelin ylävalikko.
+     *
+     */
     private JMenuBar luoValikko() {
         JMenuBar menubar = new JMenuBar();
         JMenu peli = new JMenu("Peli");
@@ -91,16 +101,26 @@ public class Kayttoliittyma extends JFrame implements Runnable {
         panel.setBackground(Color.GRAY);
 
         for (int x = 0; x < lauta.getKentanKoko(); x++) {
-            for (int y = 0; y < lauta.getKentanKoko();y++) {
+            for (int y = 0; y < lauta.getKentanKoko(); y++) {
 
                 JButton ruudunNappi = new JButton(ruudut[x][y].toString());
                 ruudunNappi.setSize(25, 25);
-                ruudunNappi.setForeground(Color.LIGHT_GRAY);
+//                ruudunNappi.setBorder(null);
+
                 panel.add(ruudunNappi);
 
-                this.kaikkiRuudut.put(ruudut[x][y], ruudunNappi);
-           }
+                HiiriKuuntelija kuuntelija = new HiiriKuuntelija(this.logiikka, this, ruudut[x][y], ruudunNappi);
+                ruudunNappi.addMouseListener(kuuntelija);
+//                this.kaikkiRuudut.put(ruudut[x][y], ruudunNappi);
+            }
         }
         return panel;
+    }
+
+    private JPanel luoAlaKentta() {
+        JPanel alaKentta = new JPanel();
+        JLabel tekstiKentta = new JLabel("tekstikenttä");
+        alaKentta.add(tekstiKentta);
+        return alaKentta;
     }
 }
